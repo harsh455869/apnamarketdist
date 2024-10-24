030b8737838507cc39ef82aeac66c1ffbbb64ebd
Harsh Patel <102902757+harsh455869@users.noreply.github.com>
2024-10-24 15:54:36 +0530

    first commit

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import { ApplicationConfig, ApnaMarketApplication } from './application';
+export * from './application';
+export declare function main(options?: ApplicationConfig): Promise<ApnaMarketApplication>;
